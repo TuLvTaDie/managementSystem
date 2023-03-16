bcfabff531295dfc82431ff66bf9fae9c820a792
TuLvTaDie <2457988393@qq.com>
2023-03-16 18:49:24 +0800

    test uploading

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <test-page></test-page>
   </div>
 </template>
+
+<script lang="ts">
+import testPage from '@/components/testPage.vue';
+export default {
+  components: {
+    testPage
+  }
+}
+</script>
+
+<style>
+
+</style>
